75c871d6166325dc2d9b0f30b82ee7b1bddf32d3
Bryan Karaffa <bryankaraffa@gmail.com>
2016-07-26 21:06:14 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
+//set headers to NOT cache a page
+header("Cache-Control: no-cache, must-revalidate"); //HTTP 1.1
+header("Pragma: no-cache"); //HTTP 1.0
 $time = time();
 if ($time /60 % 60 % 5) {
         http_response_code(403);
+        echo "Forbidden";
 }
 else {
-        echo "OK";
+      	echo "OK";
 }
 ?>
